e9872a96458df4a84ef0ab53ef2e157b4bc749b4
Archon Team <rezafardipour2@gmail.com>
2016-04-16 19:36:51 +0430

    Added files via upload

---
+++ plugins/version.lua
@@ -0,0 +1,31 @@
+do
+
+function run(msg, matches)
+  return 'NOD32-BOT v8'.. VERSION .. [[ 
+ 
+  Developer : @MOHAMMAD_NBG
+  
+  BOT: HAMINE DIGE
+  
+  
+ 
+  ZedspamBot VERSION 8
+  
+  All rights reserved.
+  __________________]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright and edit; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است
